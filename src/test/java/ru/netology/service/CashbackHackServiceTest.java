4aa7f28743a85700a30bcafb9c96359850d40207
BekkerV8 <valkenshtein@gmail.com>
2022-08-04 08:17:36 -0700

    Added junit testes and created build.gradle

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,5 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 
@@ -9,7 +7,7 @@ public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
-    @Test
+    @org.junit.Test
     public void ShouldCashback() {
         int amount = 900;
         int actual = service.remain(amount);
@@ -18,7 +16,7 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @Test
+    @org.junit.Test
     public void ShouldCashbackZero() {
         int amount = 0;
         int actual = service.remain(amount);
@@ -27,7 +25,7 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @Test
+    @org.junit.Test
     public void ShouldCashback1000() {
         int amount = 1_000;
         int actual = service.remain(amount);
@@ -36,7 +34,7 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @Test
+    @org.junit.Test
     public void ShouldCashback10() {
         int amount = 990;
         int actual = service.remain(amount);
@@ -45,8 +43,53 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @Test
+    @org.junit.Test
     public void ShouldMinusCashback() {
+        int amount = -1;
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void  JunitShouldCashback() {
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void  JunitShouldCashbackZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1_000;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void  JunitShouldCashback1000() {
+        int amount = 1_000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void  JunitShouldCashback10() {
+        int amount = 990;
+        int actual = service.remain(amount);
+        int expected = 10;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void  JunitShouldMinusCashback() {
         int amount = -1;
         int actual = service.remain(amount);
         int expected = 900;
